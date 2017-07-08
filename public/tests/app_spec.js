5de52f0a6c22e2ebe5425ae63cb2a998512de462
Tanaka Hayate <pinqphayate@gmail.com>
2017-07-08 14:28:38 +0900

    add router

---
+++ public/tests/app_spec.js
@@ -0,0 +1,12 @@
+desribe('LearnJS', function() {
+  it('can show a problem view', function() {
+    learnjs.showView('#problem-1');
+    expect($('.view-container .problem-view').length).toEqual(1);
+  });
+
+  it( 'shows the landing page view when ther is no hash', function() {
+    learnjs.showView('');
+    expect($('.view-container .landing-view').length).toEqual(1);
+
+  }
+});
