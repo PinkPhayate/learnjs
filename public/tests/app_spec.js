84524e7c2ed3424bc4fa4a4d2f7f7c05552cf210
Tanaka Hayate <pinqphayate@gmail.com>
2017-07-08 15:19:05 +0900

    showViewメソッドのパラメータ化

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -8,5 +8,27 @@ desribe('LearnJS', function() {
     learnjs.showView('');
     expect($('.view-container .landing-view').length).toEqual(1);
 
-  }
+  });
+  it( 'passes the hash view paramater to the view function', function() {
+    spyOn(learnjs, 'problemView');
+    learnjs.showView('#problem-42');
+    expect(learnjs.problemView).toHaveBeenCalledWith('42');
+  });
+  it('invokes the routes when loaded', function() {
+    spyOn(learnjs, 'showView');
+    learnjs.appOnReady();
+    expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+  });
+  it('subscribes to the hash change event', function() {
+    learnjs.appOnReady();
+    spyOn(learnjs, 'showView');
+    $(window).trigger('hashchange');
+    expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+  })
+  describe('problem view', function() {
+    it('has a tiitle that includes the problem number', function() {
+      var view = learnjs.problemView('1');
+      expect(view.text()).toEqual('Problem #1 comming soon')
+    });
+  });
 });
