7a2654d4c0631e07bdcbfa832ead104f8037adae
Tanaka Hayate <pinqphayate@gmail.com>
2017-07-08 21:12:28 +0900

    add module to bind data of problem

--- public/app.js
+++ public/app.js
@@ -1,9 +1,23 @@
 'use strict';
 var learnjs = {};
 
-learnjs.problemView = function(problemNumber) {
+//  source[learnjs/3100/public/app.js] {
+learnjs.problems = [
+  {
+    description: "What is truth?",
+    code: "function problem() { return __; }"
+  },
+  {
+    description: "Simple Math",
+    code: "function problem() { return 42 === 6 * __; }"
+  }
+];
+// }
+learnjs.problemView = function(data) {
+  var problemNumber = parseInt(data,10);
   var view = $('.templates .problem-view').clone();
-  view.find('.title').text('Problem #'+problemNumber + ' Commming soon!');
+  view.find('.title').text('Problem #'+problemNumber);
+  learnjs.applyObject(learnjs.problems[problemNumber], view);
   return view;
 }
 learnjs.showView = function(hash) {
@@ -21,4 +35,9 @@ learnjs.appOnReady = function() {
     learnjs.showView(window.location.hash);
   };
   learnjs.showView(window.location.hash);
+}
+learnjs.applyObject = function(obj, elem) {
+  for(var key in obj) {
+    elem.find('[data-name="' + key + '"]').text(obj[key]); // data-nameの属性をもつタグに値をバインディング
+  }
 }
